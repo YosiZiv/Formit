c4995c093a3c628b72e1658e29421d184a6cf18e
katlany <notan8@gmail.com>
2020-04-22 00:44:19 +0300

    all login validation

--- client/src/redux/actions/login.js
+++ client/src/redux/actions/login.js
@@ -1,10 +1,15 @@
 export const LOGIN_INPUT_CHANGE = "[login] Set Input";
+export const LOGIN_INPUT_VALIDATION = "[login] Input Validation";
 export const USER_LOGIN = "[login] user login Start";
 export const USER_LOGIN_SUCCESS = "[login] user login Success";
 export const USER_LOGIN_FAIL = "[login] user login Fail";
 
 export const loginInputChange = (payload) => ({
   type: LOGIN_INPUT_CHANGE,
+  payload,
+});
+export const loginInputValidation = (payload) => ({
+  type: LOGIN_INPUT_VALIDATION,
   payload,
 });
 
