95f4b4b7cc5b877893607f6bf3271c492ce9c77b
katlany <notan8@gmail.com>
2020-04-21 05:20:05 +0300

    add all page to app test react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Main } from "./components/pages/Main";
+import { Login } from "./components/pages/Login";
+import { Register } from "./components/pages/Register";
+import { FormBuild } from "./components/pages/FormBuild";
+import { FormsLists } from "./components/pages/FormsLists";
+import { FormsSubmissions } from "./components/pages/FormsSubmissions";
+import { FormSubmission } from "./components/pages/FormSubmission";
 import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>formit app start</h1>
-    </div>
+  const routes = (
+    <>
+      <Switch>
+        <Route path='/' component={Main} exact />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/formbuild' component={FormBuild} />
+        <Route path='/formslists' component={FormsLists} />
+        <Route path='/formssubmissions' component={FormsSubmissions} />
+        <Route path='/formsubmission' component={FormSubmission} />
+      </Switch>
+    </>
   );
+  return <div className='App'>{routes}</div>;
 }
 
 export default App;
