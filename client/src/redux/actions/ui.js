dff41773f7027ced5ba65c57db6c4beb593b6fe0
katlany <notan8@gmail.com>
2020-04-22 06:59:12 +0300

    create ui actions && reducers

---
+++ client/src/redux/actions/ui.js
@@ -0,0 +1,29 @@
+export const SET_MESSAGE = "[UI] Message";
+export const DELETE_MESSAGE = "[UI] Delete Message";
+export const REDIRECT = "[UI] Redirect";
+export const CLEAR_UI = "[UI] Clear UI State";
+export const LOADING_START = "[UI] Loading Start";
+export const LOADING_FINISH = "[UI] Loading Finish";
+export const setMessage = (payload) => ({
+  type: SET_MESSAGE,
+  payload,
+});
+export const deleteMessage = () => {
+  console.log("delete message action");
+  return {
+    type: DELETE_MESSAGE,
+  };
+};
+export const redirectTo = (url) => ({
+  type: REDIRECT,
+  payload: url,
+});
+export const loadingStart = () => ({
+  type: LOADING_START,
+});
+export const loadingFinish = () => ({
+  type: LOADING_FINISH,
+});
+export const clearUi = () => ({
+  type: CLEAR_UI,
+});
---
+++ client/src/redux/reducers/ui.js
@@ -0,0 +1,33 @@
+import {
+  SET_MESSAGE,
+  DELETE_MESSAGE,
+  REDIRECT,
+  CLEAR_UI,
+  LOADING_START,
+  LOADING_FINISH,
+} from "../actions/ui";
+const initState = {
+  message: null,
+  redirect: null,
+  loading: false,
+};
+
+export default function ui(state = initState, action) {
+  switch (action.type) {
+    case REDIRECT:
+      return { ...state, redirect: action.payload };
+    case SET_MESSAGE:
+      return { ...state, message: action.payload };
+    case DELETE_MESSAGE:
+      console.log("delete message reducer");
+      return { ...state, message: null };
+    case CLEAR_UI:
+      return { ...state, loading: false, message: {}, redirect: null };
+    case LOADING_START:
+      return { ...state, loading: true };
+    case LOADING_FINISH:
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+}
