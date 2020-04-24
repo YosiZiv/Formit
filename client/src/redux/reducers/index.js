f9fff8bf36ceab4b9fb38f5bdddef5ed0e8d1360
katlany <notan8@gmail.com>
2020-04-24 17:20:12 +0300

    add register to index

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import login from "./login";
 import ui from "./ui";
 import formBuild from "./formBuild";
+import register from "./register";
 export const reducers = combineReducers({
   formBuild,
   login,
   ui,
+  register,
 });
