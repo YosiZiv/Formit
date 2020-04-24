e982fe388bed80712fc4a7df96b810bcae4c2d07
katlany <notan8@gmail.com>
2020-04-24 11:27:56 +0300

    add authorization header

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,4 +3,5 @@ import Axios from "axios";
 // "http://ec2-18-185-92-148.eu-central-1.compute.amazonaws.com/api"  production server
 export const axios = Axios.create({
   baseURL: "http://localhost:4000/api/v1",
+  headers: { authorization: localStorage.getItem("token") },
 });
