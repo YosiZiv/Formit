3d4ed5c91cceab63a3e2c00d73831bee5d855bf7
katlany <notan8@gmail.com>
2020-04-25 17:05:24 +0300

    redux rename formBuild to form

--- client/src/redux/middlewares/form.js
+++ client/src/redux/middlewares/form.js
@@ -3,8 +3,11 @@ import {
   FORM_SUBMIT,
   FORM_SUBMIT_SUCCESS,
   FORM_SUBMIT_FAIL,
-} from "../actions/formBuild";
-import { setMessage, deleteMessage, redirect } from "../actions/ui";
+  GET_FORM,
+  GET_FORM_SUCCESS,
+  GET_FORM_FAIL,
+} from "../actions/form";
+import { setMessage, redirect } from "../actions/ui";
 
 const formSubmit = ({ dispatch }) => (next) => (action) => {
   if (action.type === FORM_SUBMIT) {
@@ -37,4 +40,32 @@ const formSubmitFail = ({ dispatch }) => (next) => (action) => {
   }
   next(action);
 };
-export const formMdl = [formSubmit, formSubmitSuccess, formSubmitFail];
+
+const getForm = ({ dispatch }) => (next) => (action) => {
+  if (action.type === GET_FORM) {
+    const URL = "/form" + action.payload;
+    return dispatch(apiRequest("get", URL, GET_FORM_SUCCESS, GET_FORM_FAIL));
+  }
+  next(action);
+};
+
+const getFormSuccess = ({ dispatch }) => (next) => (action) => {
+  if (action.type === GET_FORM) {
+    console.log(action.payload);
+  }
+  next(action);
+};
+const getFormFail = ({ dispatch }) => (next) => (action) => {
+  if (action.type === GET_FORM) {
+    console.log(action.payload);
+  }
+  next(action);
+};
+export const formMdl = [
+  formSubmit,
+  formSubmitSuccess,
+  formSubmitFail,
+  getForm,
+  getFormSuccess,
+  getFormFail,
+];
