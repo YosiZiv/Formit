bbe23f4708d38683a26158ebc047409c9da0e213
katlany <notan8@gmail.com>
2020-04-26 20:52:00 +0300

    fix bug

--- server/env.js
+++ server/env.js
@@ -1,9 +1,9 @@
 const {
-  env: { MongoURI, SECRET, TOKEN_EXPIRES },
+  env: { MONGO_URI, SECRET, TOKEN_EXPIRES },
 } = process;
 module.exports = {
   //  MongoDB MLab Credentials
   TOKEN_EXPIRES,
   SECRET,
-  MongoURI,
+  MONGO_URI,
 };
