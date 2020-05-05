cc6493c8c87fbee81a8de9401ae5e1b6a0febb54
katlany <notan8@gmail.com>
2020-05-05 05:39:06 +0300

    connect spinner to redux loading

--- client/src/components/layouts/Spinner.js
+++ client/src/components/layouts/Spinner.js
@@ -1,10 +1,16 @@
 import React from "react";
-
-const Spinner = () => (
-  <div className='d-flex justify-content-center'>
-    <div className='spinner-border text-primary text-center m-5'>
-      <span className='sr-only'>Loading...</span>
+import { connect } from "react-redux";
+const Spinner = ({ loading }) => {
+  const loadingUi = loading && (
+    <div className='d-flex justify-content-center'>
+      <div className='spinner-border text-primary text-center m-5'>
+        <span className='sr-only'>Loading...</span>
+      </div>
     </div>
-  </div>
-);
-export default Spinner;
+  );
+  return loadingUi;
+};
+const mapStateToProps = ({ ui: { loading } }) => {
+  return { loading };
+};
+export default connect(mapStateToProps)(Spinner);
