112ec57571479b74ef38d5962e9a373cd08f9f13
katlany <notan8@gmail.com>
2020-05-05 05:46:39 +0300

    add server validation for controller

--- server/api/v1/controllers/user.js
+++ server/api/v1/controllers/user.js
@@ -1,41 +1,42 @@
 const { User, Form } = require("../../../models/index");
 const bcrypt = require("bcryptjs");
-
+const { createUserValidation } = require("../../../core/validations");
 exports.createUser = async (req, res) => {
   const { body } = req;
-  //   const errors = validateAdminRegisterInput(body);
-  //   if (Object.keys(errors).length) {
-  //     return res.status(400).json({ errors });
-  //   }
+  const errors = createUserValidation(body);
+  if (Object.keys(errors).length) {
+    return res.status(400).json({ errors });
+  }
   try {
     const newUser = await new User({ ...body });
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(newUser.password, salt, async (e, hash) => {
         if (e) {
-          return res
-            .status(401)
-            .json({ error: `something went wrong here :/` });
+          errors.server = "something went wrong here :/";
+          return res.status(500).json({ errors });
         }
         newUser.password = hash;
         newUser
           .save()
           .then((createdUser) => {
-            return res
-              .status(200)
-              .json({ msg: `User Created Successfully`, data: createdUser });
+            return res.status(200).json({ data: createdUser });
           })
           .catch((e) => {
             if (e.code === 11000) {
-              return res.status(401).json({ error: `Email Have To be Unique` });
+              errors.email = "Email Have To be Unique";
+              return res.status(401).json({ errors });
             }
-            return res.status(500).json({ error: "something went wrong :/" });
+            errors.server = "something went wrong :/";
+            return res.status(500).json({ errors });
           });
       });
     });
   } catch (e) {
-    return res.status(401).json({ error: "someting went wrong :/" });
+    errors.server = "something went wrong :/";
+    return res.status(500).json({ errors });
   }
 };
+
 exports.getAllFormsByUserId = async (req, res) => {
   const {
     decoded: {
